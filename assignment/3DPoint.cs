741de0458ee880c2adda14e1c84c63f0a7c554a8
Hana <hanamarwasadek2004@gmail.com>
2025-08-11 18:55:56 +0300

    solution of Project01

---
+++ assignment/3DPoint.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class _3DPoint:ICloneable,IComparable<_3DPoint>
+    {
+        #region Properties
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public _3DPoint()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+        public _3DPoint(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public _3DPoint(int x):this(x,0,0)
+        {
+          
+        }
+        public _3DPoint(int x,int y) : this(x, y, 0)
+        {
+
+        }
+
+        public _3DPoint(_3DPoint p)
+        {
+            X = p.X;
+            Y = p.Y;
+            Z = p.Z;
+
+        }
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public object Clone()
+        {
+            return new _3DPoint(this);
+        }
+
+        public int CompareTo(_3DPoint? other)
+        {
+            if (other is not null)
+            {
+                if (X != other.X)
+                    return X.CompareTo(other.X);
+                return Y.CompareTo(other.Y);
+
+            }
+            return 1;
+ 
+        }
+
+        #endregion
+
+        #region OperatorOverLoading
+
+        public static bool operator ==(_3DPoint? p1, _3DPoint? p2)
+        {
+            if (p1 is not null && p2 is not null)
+                return p1.X == p2.X && p1.Y == p2.Y && p1.Z == p2.Z;
+            
+            return true;
+        }
+
+        public static bool operator !=(_3DPoint? p1, _3DPoint? p2) {
+            return !(p1 == p2);
+        }
+
+
+
+
+
+        #endregion
+    }
+}
---
+++ assignment/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Project01
+
+            //_3DPoint P = new _3DPoint(1, 2, 3);
+
+            //Console.WriteLine(P.ToString());
+
+            //_3DPoint P1 = new _3DPoint();
+
+            //_3DPoint P2 = new _3DPoint();
+
+            //bool flag= int.TryParse(Console.ReadLine(), out int x);
+            //if (flag)
+            //{
+            //    P1 = new _3DPoint(x);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input for x coordinate.");
+            //}
+
+            //flag = int.TryParse(Console.ReadLine(), out int y);
+
+            //if (flag)
+            //{
+            //    P2 = new _3DPoint(x, y);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input for y coordinate.");
+            //}
+
+            //if(P1 == P2)
+            //{
+            //    Console.WriteLine("P1 and P2 are equal.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("P1 and P2 are not equal.");
+            //}
+
+
+            //P1=(_3DPoint) P2.Clone();
+
+            //_3DPoint[] points =
+            //        {
+            //        P1,
+            //        P2,
+            //        new _3DPoint(5, 2, 3),
+            //        new _3DPoint(1, 2, 3),
+            //        new _3DPoint(5, 1, 7)
+            //        };
+
+            //Array.Sort(points);
+
+            //Console.WriteLine("Sorted Points:");
+
+            //foreach (_3DPoint point in points)
+            //{
+            //    Console.WriteLine(point);
+            //}
+
+
+
+
+
+
+            #endregion
+
+        }
+    }
+}
